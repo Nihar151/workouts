3840b88167e2cf79bca9c05be2e126aa5da6bf98
Nihar Pillai <gangadharannihar@gmail.com>
2026-06-29 10:36:04 +0530

    Signup user logic done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,15 @@ const loginUser = async(req, res)=>{
 
 //signup user
 const signupUser = async(req, res)=>{
-    res.json({msg:"Signed up"})
+
+    const {username, email, password} = req.body
+    try {
+        const user = await User.signup(username, email, password)
+        res.status(200).json({username, email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
 }
 
 module.exports = {signupUser, loginUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     username:{
         type:String,
-        required: true
+        required: true,
+        unique:true
     },
     email:{
         type:String,
@@ -17,5 +20,42 @@ const userSchema = new Schema({
         required:true
     }
 }, {timestamps:true})
+
+//static signup func
+
+userSchema.statics.signup = async function(username, email, password){
+
+    //validation
+    if(!email || !password || !username){
+        throw Error("All fields must be filled")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Email is not valid")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough")
+    }
+    if(username.length<3){
+        throw Error("username should be longer")
+    }
+
+
+
+    const emailExists = await this.findOne({email})
+    const usernameExists = await this.findOne({username})
+
+    if(emailExists){
+        throw Error("Email already exists")
+    }
+    if(usernameExists){
+        throw Error("Username already in use")
+    }
+    
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    const user = await this.create({username, email, password:hash})
+    return user
+}
 
 module.exports = mongoose.model("User", userSchema)
